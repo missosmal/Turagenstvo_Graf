ac88fc3ff6857d607fc480f932ced2dd92722edb
missosmal <fadesteam8@gmail.com>
2024-02-22 04:00:14 +0500

    Прописан код MainWindow.xaml.cs, создан класс для конекта к бд.Graf

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -9,6 +10,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -20,9 +22,61 @@ namespace Turagenstvo_Graf
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static MainWindow init;
+        public static Connection connect;
+        public static Pages.Main main;
+        public static Login_win login;
         public MainWindow()
         {
             InitializeComponent();
+            init = this;
+            connect = new Connection();
+            main = new Pages.Main();
+            login = new Login_win();
+            OpenPageLogin();
+        }
+
+
+        public void OpenPageLogin()
+        {
+            DoubleAnimation opgridAnimation = new DoubleAnimation();
+            opgridAnimation.From = 1;
+            opgridAnimation.To = 0;
+            opgridAnimation.Duration = TimeSpan.FromSeconds(0.6);
+            opgridAnimation.Completed += delegate
+            {
+                frame.Navigate(login);
+
+                DoubleAnimation opgtisdAnimation = new DoubleAnimation();
+                opgtisdAnimation.From = 0;
+                opgtisdAnimation.To = 1;
+                opgtisdAnimation.Duration = TimeSpan.FromSeconds(1.2);
+
+                frame.BeginAnimation(Frame.OpacityProperty, opgtisdAnimation);
+            };
+
+            frame.BeginAnimation(Frame.OpacityProperty, opgridAnimation);
+        }
+
+        public void OpenPageMain()
+        {
+            DoubleAnimation opgridAnimation = new DoubleAnimation();
+            opgridAnimation.From = 1;
+            opgridAnimation.To = 0;
+            opgridAnimation.Duration = TimeSpan.FromSeconds(0.6);
+            opgridAnimation.Completed += delegate
+            {
+                frame.Navigate(main);
+
+                DoubleAnimation opgtisdAnimation = new DoubleAnimation();
+                opgtisdAnimation.From = 0;
+                opgtisdAnimation.To = 1;
+                opgtisdAnimation.Duration = TimeSpan.FromSeconds(1.2);
+
+                frame.BeginAnimation(Frame.OpacityProperty, opgtisdAnimation);
+            };
+
+            frame.BeginAnimation(Frame.OpacityProperty, opgridAnimation);
         }
     }
 }
